95f6d75caaca81ea3f6fe4188faaf2591dd7eb74
Thao Ha <thaoanhhaa1@gmail.com>
2022-08-03 16:42:11 +0700

    PageNotFound - Responsive Phần 1

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -7,15 +7,17 @@ const PageNotFound = () => {
     useBackToTop();
 
     return (
-        <section className="py-10 bg-white font-['Arvo',_serif]">
+        <section className="min-h-screen max-w-5xl py-10 flex items-center justify-center bg-white font-['Arvo',_serif]">
             <div className="container mx-auto">
                 <div className="col-sm-10 col-sm-offset-1  text-center">
                     <div className="bg-404-page h-[400px] bg-center">
                         <h1 className="text-[80px]">404</h1>
                     </div>
 
-                    <div className="-mt-[50px]">
-                        <h3 className="text-[80px]">Look like you're lost</h3>
+                    <div className="-mt-[50px] px-8">
+                        <h3 className="text-4xl xs:text-[80px] xs:leading-tight">
+                            Look like you're lost
+                        </h3>
 
                         <p>the page you are looking for not avaible!</p>
 
