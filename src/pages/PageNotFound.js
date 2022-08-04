9388aad20dcb691e9d933447b778d477c4e379b9
Thao Ha <thaoanhhaa1@gmail.com>
2022-08-04 22:44:44 +0700

    Fix Edit Personal Information

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -7,9 +7,9 @@ const PageNotFound = () => {
     useBackToTop();
 
     return (
-        <section className="min-h-screen max-w-5xl py-10 flex items-center justify-center bg-white font-['Arvo',_serif]">
+        <section className="min-h-screen max-w-5xl mx-auto py-10 flex items-center justify-center bg-white font-['Arvo',_serif]">
             <div className="container mx-auto">
-                <div className="col-sm-10 col-sm-offset-1  text-center">
+                <div className="col-sm-10 col-sm-offset-1 text-center">
                     <div className="bg-404-page h-[400px] bg-center">
                         <h1 className="text-[80px]">404</h1>
                     </div>
